384a8dd1512b259a625c6c1d55911547e2842e21
debaditya <debaditb@thougtworks.com>
2015-06-05 03:59:03 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to codurance-project
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/codurance_project/core.clj
@@ -0,0 +1,7 @@
+(ns codurance-project.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
